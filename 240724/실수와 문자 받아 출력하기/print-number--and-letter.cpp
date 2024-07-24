7df6e4764a0572801ec22646d3c7d312dac4f9e5
Kimayoung <157570693+gdhsf3701@users.noreply.github.com>
2024-07-24 23:12:11 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a ;
+    double b , c; 
+
+    cin  >> a >>  b >> c;
+    cout.precision(2);
+    cout << fixed;
+
+    cout << a << endl << b << endl << c;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
