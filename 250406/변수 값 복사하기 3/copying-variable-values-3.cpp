9527c0bfe044ab01f766214731f3aa75f29a563b
Kimayoung <157570693+gdhsf3701@users.noreply.github.com>
2025-04-06 23:36:35 +0900

    Title: 변수 값 복사하기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250406/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 1;
+    int b= 5;
+    int c = 3;
+    int temp;
+
+    temp = c;
+    c = a;
+    a = a+temp;
+    b = b - temp;
+    return 0;
+}
